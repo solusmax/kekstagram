78e2992636aaaa1f1b5748b6490a39024a61a754
solusmax <solusmaxim@gmail.com>
2021-02-02 00:33:29 +0300

    Включил строгий режим

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Получить рандомное натуральное число (включая 0) из диапазона
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
 
