7366c1cc4ad7b3dcf915190d281f78252eb5b76d
solusmax <solusmaxim@gmail.com>
2021-02-01 14:58:06 +0300

    Добавил проверку на слишком узкий диапазон

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
 
 const getRandomInteger = (min, max) => {
-  if (min < 0 || max < 0) {
+  if (min < 0 || max < 0 || (Math.trunc(min) === Math.trunc(max) && !Number.isInteger(min) && !Number.isInteger(max))) {
+    // Последняя часть условия — проверка,
+    // не создают ли параметры диапазон вроде (1.5, 1.6)
     return;
   }
 
