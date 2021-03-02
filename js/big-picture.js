cf1858ddb244b8fdf4ff279e4b51dc2dfe6f2272
solusmax <solusmaxim@gmail.com>
2021-03-02 22:56:10 +0300

    Убрал избыточный префикс у названий переменных

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,16 +4,16 @@ import {isEscEvent} from './util.js';
 const pictures = document.querySelectorAll('.picture');
 
 const bigPictureNode = document.querySelector('.big-picture');
-const bigPictureCloseNode = bigPictureNode.querySelector('#picture-cancel');
-const bigPictureImgNode = bigPictureNode.querySelector('.big-picture__img img');
-const bigPictureDescriptionNode = bigPictureNode.querySelector('.social__caption');
-const bigPictureLikesCounterNode = bigPictureNode.querySelector('.likes-count');
-const bigPictureCommentsCounterNode = bigPictureNode.querySelector('.comments-count');
-const bigPictureCommentsNode = bigPictureNode.querySelector('.social__comments');
+const closeButtonNode = bigPictureNode.querySelector('#picture-cancel');
+const imgNode = bigPictureNode.querySelector('.big-picture__img img');
+const descriptionNode = bigPictureNode.querySelector('.social__caption');
+const likesCounterNode = bigPictureNode.querySelector('.likes-count');
+const commentsCounterNode = bigPictureNode.querySelector('.comments-count');
+const commentsNode = bigPictureNode.querySelector('.social__comments');
 
 // Для временного скрытия
-const bigPictureSocialCommentCountNode = bigPictureNode.querySelector('.social__comment-count');
-const bigPictureCommentsLoaderNode = bigPictureNode.querySelector('.comments-loader');
+const socialCommentCountNode = bigPictureNode.querySelector('.social__comment-count');
+const commentsLoaderNode = bigPictureNode.querySelector('.comments-loader');
 
 const onBigPictureCloseClick = () => {
   closeBigPicture();
@@ -31,7 +31,7 @@ const openBigPicture = () => {
   bigPictureNode.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
-  bigPictureCloseNode.addEventListener('click', onBigPictureCloseClick);
+  closeButtonNode.addEventListener('click', onBigPictureCloseClick);
   document.addEventListener('keydown', onBigPictureEscKeydown);
 }
 
@@ -39,19 +39,19 @@ const closeBigPicture = () => {
   bigPictureNode.classList.add('hidden');
   document.body.classList.remove('modal-open');
 
-  bigPictureCloseNode.removeEventListener('click', onBigPictureCloseClick);
+  closeButtonNode.removeEventListener('click', onBigPictureCloseClick);
   document.removeEventListener('keydown', onBigPictureEscKeydown);
 }
 
 const renderBigPicture = ({url, likes, comments, description}) => {
-  bigPictureImgNode.src = url;
-  bigPictureLikesCounterNode.textContent = likes;
-  bigPictureCommentsCounterNode.textContent = comments.length;
-  bigPictureDescriptionNode.textContent = description;
+  imgNode.src = url;
+  likesCounterNode.textContent = likes;
+  commentsCounterNode.textContent = comments.length;
+  descriptionNode.textContent = description;
 
   // Временное скрытие
-  bigPictureSocialCommentCountNode.classList.add('hidden');
-  bigPictureCommentsLoaderNode.classList.add('hidden');
+  socialCommentCountNode.classList.add('hidden');
+  commentsLoaderNode.classList.add('hidden');
 }
 
 const createCommentHtml = ({avatar, message, name}) => {
@@ -69,10 +69,10 @@ const createCommentHtml = ({avatar, message, name}) => {
 }
 
 const renderComments = (comments) => {
-  bigPictureCommentsNode.innerHTML = '';
+  commentsNode.innerHTML = '';
 
   comments.forEach((comment) => {
-    bigPictureCommentsNode.insertAdjacentHTML('beforeend', createCommentHtml(comment));
+    commentsNode.insertAdjacentHTML('beforeend', createCommentHtml(comment));
   });
 }
 
