48a739a4bb4fdd86478dd1c214e3fdab91c47d78
solusmax <solusmaxim@gmail.com>
2021-03-15 23:52:39 +0300

    Отрефакторил сообщения об ошибках загрузки
    Заменил константы одним словарём.

--- js/data.js
+++ js/data.js
@@ -3,8 +3,10 @@ import { showErrorMessage } from './util.js';
 const PHOTOS_DATA_URL = 'https://22.javascript.pages.academy/kekstagram/data';
 const SENDING_PHOTO_URL = 'https://22.javascript.pages.academy/kekstagram';
 
-const ERROR_LOADING_MESSAGE = 'Ошибка загрузки изображений'
-const ERROR_SENDING_MESSAGE = 'Ошибка загрузки файла';
+const errorMessages = {
+  loading: 'Ошибка загрузки изображений',
+  sending: 'Ошибка загрузки файла',
+}
 
 const getPhotosData = (onSuccess) => {
   fetch(PHOTOS_DATA_URL)
@@ -13,7 +15,7 @@ const getPhotosData = (onSuccess) => {
         return response.json()
       }
 
-      throw new Error(ERROR_LOADING_MESSAGE);
+      throw new Error(errorMessages.loading);
     })
     .then((photos) => {
       onSuccess(photos);
@@ -32,7 +34,7 @@ const sendPhotoData = (onSuccess, onError, body) => {
       if (response.ok) {
         onSuccess();
       } else {
-        throw new Error(ERROR_SENDING_MESSAGE);
+        throw new Error(errorMessages.sending);
       }
     })
     .catch(() => {
